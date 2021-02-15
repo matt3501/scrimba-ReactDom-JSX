6d2702c1ab2dec6c5c1b777f091c101b8db8d7e2
Matt Plank <matthew.plank@generac.com>
2021-02-15 13:14:05 -0600

    Small update from end of lesson

--- lesson1/src/index.js
+++ lesson1/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // JSX - looks almost identical to HTML
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"))
+ReactDOM.render(<div><h1>Hello World</h1><p>This is a paragraph</p></div>, document.getElementById("root"))
 
 
